39e7fece09543637b044dd29894ebbba2a06fa51
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2025-05-17 13:56:57 +0200

    Removed unused files and code

--- autoloads/event_bus.gd
+++ autoloads/event_bus.gd
@@ -1,7 +1,5 @@
 extends Node
 
-#signal execute_action(caster: Unit, origin: Vector2i, target: Vector2i, action: Action);
-
 signal hide_actions;
 signal show_actions(actions: Array[Action]);
 signal action_chosen(action: Action, position: Vector2i);
@@ -12,7 +10,6 @@ signal clear_area_highlights(area: Array[Vector2i]);
 
 signal highlight_area(primary: Vector2i, area: Array[Vector2i], color: Color);
 signal highlight_radius(center: Vector2i, radius: int, color: Color);
-
 
 signal set_winner(team: UnitTeam);
 signal set_winner_draw;
